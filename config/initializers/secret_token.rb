d8784ef16ce704d3f568828bdead8113d276eb4c
Vijay <crazyraw@gmail.com>
2011-06-01 17:46:12 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vrails::Application.config.secret_token = 'd7f94ccf16dc68a2ab5b99c562c4e144d27967e1198f6e02b74705838bd5316be1b5d770a5c6518354c8766805a232510925ac6c0809f25096a7e18d34678974'
